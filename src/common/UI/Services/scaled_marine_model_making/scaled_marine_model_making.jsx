415e5bcaaf54f852ff0bcfbf8ec914db48611cc6
DareDevilStudios <studios.daredevil@gmail.com>
2024-02-26 18:00:58 +0530

    datas setting done

--- src/common/UI/Services/scaled_marine_model_making/scaled_marine_model_making.jsx
+++ src/common/UI/Services/scaled_marine_model_making/scaled_marine_model_making.jsx
@@ -9,9 +9,9 @@ import { useRouter } from "next/router";
 // import json for data
 import { services } from "../../../../../data.json";
 
-const scaled_marine_model_making = ({slugMatcher}) => {
+const scaled_marine_model_making = ({ slugMatcher }) => {
   const router = useRouter();
-  const {service} = router.query;
+  const { service } = router.query;
 
   console.log("slug is : " + service);
   // set the data state variable to array of services having the slug same as the url parameter
@@ -25,30 +25,57 @@ const scaled_marine_model_making = ({slugMatcher}) => {
 
   if (!data || data.length === 0) {
     // Handle the case when data is not available
-    return <div>No data found</div>;
+    return (
+      <div>
+        <section class="bg-white dark:bg-gray-900">
+          <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+            <div class="mx-auto max-w-screen-sm text-center">
+              <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
+                404
+              </h1>
+              <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+                Something's missing.
+              </p>
+              <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
+                Sorry, we can't find that page. You'll find lots to explore on
+                the home page.{" "}
+              </p>
+              <a
+                href="#"
+                class="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
+              >
+                Back to Homepage
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
+    );
   }
 
   console.log("data: " + JSON.stringify(data[0]));
   console.log("data: " + JSON.stringify(data[0].main_title));
- 
+
   return (
     <div className="">
       <Navbar />
       <Mtop />
       <Banner_services title={data[0].main_title} />
       <div className="p-12">
-        <div class="w-full p-7 md:p-12 flex md:flex-row flex-col justify-between items-center gap-12">
-          <p class="text-justify md:w-1/2 font-sm text-gray-500 ">
-            {data[0].content}
-          </p>
-          <Image
-            className="md:w-1/2"
-            src={data[0].main_image_url}
-            alt="About"
-            width={300}
-            height={500}
-          />
-        </div>
+        {data[0]?.main_image_url && (
+          <div class="w-full p-7 md:p-12 flex md:flex-row flex-col justify-between items-center gap-12">
+            <p class="text-justify md:w-1/2 font-sm text-gray-500 ">
+              {data[0].content}
+            </p>
+            <Image
+              className="md:w-1/2"
+              src={data[0].main_image_url}
+              alt="About"
+              width={300}
+              height={500}
+            />
+          </div>
+        )}
         {data[0]?.services_provided?.map((service) => (
           <div class="flex flex-col items-center justify-center w-full h-full">
             <h1 class="font-body w-full mb-5 text-2xl font-bold tracking-wide leading-1 text-blue-700">
@@ -61,9 +88,9 @@ const scaled_marine_model_making = ({slugMatcher}) => {
             <div className="flex flex-wrap justify-center gap-3">
               {service.images.map((item) => (
                 <Image
-                  className="w-64"
+                  className="w-96"
                   width={300}
-                  height={300}
+                  height={500}
                   src={item}
                   alt="About"
                 />
